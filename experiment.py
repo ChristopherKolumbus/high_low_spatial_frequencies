d34a098a36fc74d70374b9717accb18ca3074699
ChristopherKolumbus <33834216+ChristopherKolumbus@users.noreply.github.com>
2018-01-18 17:15:14 +0100

    Changed how experimental conditions are determined

--- experiment.py
+++ experiment.py
@@ -1,46 +1,38 @@
-from random import shuffle, randint
+from random import shuffle
 
-places = ['0', '1', '2']
-pictures = ['0', '1', '2']
-repetitions = 12
+repetitions = 3
+number_places = 3
+number_pictures = 3
+number_letters = 2
+number_orientations = 2
 base_angle = 180
+diff_angle = 30
 
 conditions = []
-count = 0
 for repetition in range(repetitions):
-    for place in places:
-        for picture in pictures:
-            if count % 2 == 0:
-                conditions.append([place, picture, base_angle + 30])
-            else:
-                conditions.append([place, picture, base_angle - 30])
-            count += 1
+    for place in range(number_places):
+        for picture in range(number_pictures):
+            for letter in range(number_letters):
+                for orientation in range(number_orientations):
+                    if orientation == 0:
+                        condition = [place, picture, letter, base_angle + diff_angle]
+                    else:
+                        condition = [place, picture, letter, base_angle - diff_angle]
+                    conditions.append(condition)
 
 shuffle(conditions)
 
-place_temp = []
-picture_temp = []
-angle_temp = []
+places = []
+pictures = []
+letters = []
+orientations = []
 for condition in conditions:
-    place, picture, angle = condition
-    place_temp.append(place)
-    picture_temp.append(picture)
-    angle_temp.append(str(angle))
-place_output = ', '.join(place_temp)
-picture_output = ', '.join(picture_temp)
-angle_output = '.0f, '.join(angle_temp)
-#print(place_output)
-#print(picture_output)
-#print(angle_output)
+    places.append(str(condition[0]))
+    pictures.append(str(condition[1]))
+    letters.append(str(condition[2]))
+    orientations.append(str(condition[3]))
 
-scale_shift = []
-for i in range(len(conditions)):
-    scale_shift.append(str(randint(1, 4)))
-scale_shift_output = ', '.join(scale_shift)
-#print(scale_shift_output)
-
-e_or_f = []
-for i in range(len(conditions)):
-    e_or_f.append(str(randint(0, 1)))
-e_or_f_output = ', '.join(e_or_f)
-print(e_or_f_output)
+print('Place: ' + ', '.join(places))
+print('Picture: ' + ', '.join(pictures))
+print('Letter: ' + ', '.join(letters))
+print('Orientation: ' + '.0f, '.join(orientations))
